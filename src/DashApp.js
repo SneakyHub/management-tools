5fe8615ed08364a11b601d9c2ccc561f56a5d4b3
DarkGuy10 <HarperBurn@protonmail.ch>
2021-07-26 11:45:29 +0530

    Adding functions

--- src/DashApp.js
+++ src/DashApp.js
@@ -1,9 +1,80 @@
-const Application = require("./Application");
+const Application = require("./Application")
+const FormData = require('form-data')
 
 class DashApp extends Application {
     constructor(host, key) {
         super(host, key)
     }
+
+    async getUserByDiscordId(discord_id) {
+        const user = (await this.instance.get(`/api/users/${discord_id}`)).data
+        return user
+    }
+
+    async getCredits(discord_id) {
+        const credits = (await this.getUserByDiscordId(discord_id))['credits']
+        return credits
+    }
+
+    async setCredits(discord_id, credits){
+        const {name, email, server_limit, role} = await this.getUserByDiscordId(discord_id)
+
+        let data = new FormData()
+        data.append('name', name)
+        data.append('email', email)
+        data.append('credits', credits)
+        data.append('server_limit', server_limit)
+        data.append('role', role)
+
+        await this.instance.patch(`/api/users/${discord_id}`, data, {
+            headers: {
+                ...data.getHeaders()
+            }
+        }).then(() => console.log(`[ > ] Set credits to ${deltaCredits} for user [discord_id = ${discord_id}]`))
+            .catch((error) => console.log(`[ * ] Error while setting credits for user [discord_id = ${discord_id} \n Traceback: \n ${error}`))
+    }
+
+    async addCredits(discord_id, deltaCredits){
+        const {name, email, credits, server_limit, role} = await this.getUserByDiscordId(discord_id)
+        const newCredits = credits + deltaCredits
+
+        let data = new FormData()
+        data.append('name', name)
+        data.append('email', email)
+        data.append('credits', newCredits)
+        data.append('server_limit', server_limit)
+        data.append('role', role)
+
+        await this.instance.patch(`/api/users/${discord_id}`, data, {
+            headers: {
+                ...data.getHeaders()
+            }
+        }).then(() => console.log(`[ > ] Added ${deltaCredits} to user [discord_id = ${discord_id}]`))
+            .catch((error) => console.log(`[ * ] Error while adding credits to user [discord_id = ${discord_id} \n Traceback: \n ${error}`))
+    }
+
+    async subtractCredits(discord_id, deltaCredits){
+        const {name, email, credits, server_limit, role} = await this.getUserByDiscordId(discord_id)
+        const newCredits = credits - deltaCredits
+
+        if(newCredits < 0)
+            throw new Error(`User does not have enough credits!`)
+
+        let data = new FormData()
+        data.append('name', name)
+        data.append('email', email)
+        data.append('credits', newCredits)
+        data.append('server_limit', server_limit)
+        data.append('role', role)
+
+        await this.instance.patch(`/api/users/${discord_id}`, data, {
+            headers: {
+                ...data.getHeaders()
+            }
+        }).then(() => console.log(`[ > ] Subtracted ${deltaCredits} from user [discord_id = ${discord_id}]`))
+            .catch((error) => console.log(`[ * ] Error while subtracting credits from user [discord_id = ${discord_id} \n Traceback: \n ${error}`))
+    }
+
 }
 
 module.exports = DashApp
--- src/PanelApp.js
+++ src/PanelApp.js
@@ -10,6 +10,11 @@ class PanelApp extends Application {
         return servers
     }
 
+    async getServerById(server_id) {
+        const server = (await this.instance.get(`/api/application/servers/${server_id}`))['attributes']
+        return server
+    }
+
     async getServersByEgg(egg_id) {
         const servers = (await this.getAllServers()).filter( server => server.egg == egg_id )
         return servers
@@ -17,6 +22,11 @@ class PanelApp extends Application {
 
     async getServersByNest(nest_id) {
         const servers = (await this.getAllServers()).filter( server => server.nest == nest_id )
+        return servers
+    }
+
+    async getServersByUser(user_id) {
+        const servers = (await this.instance.get(`/api/application/users/${user_id}?include=servers`)).data['attributes']['relationships']['servers']['data'].map(x => x.attributes)
         return servers
     }
 
@@ -30,6 +40,12 @@ class PanelApp extends Application {
         await this.instance.delete(`/api/application/servers/${server.id}/force`)
         .then(() => console.log(`[ > ] Successfully deleted server [ID = ${server.id}]`))
         .catch(error => console.log(`[ * ] Error in deleting server [ID = ${server.id}] \n Traceback: \n ${error}`))
+    }
+
+    async deleteServerById(server_id) {
+        await this.instance.delete(`/api/application/servers/${server_id}/force`)
+        .then(() => console.log(`[ > ] Successfully deleted server [ID = ${server_id}]`))
+        .catch(error => console.log(`[ * ] Error in deleting server [ID = ${server_id}] \n Traceback: \n ${error}`))
     }
 
     async deleteServersByEgg(egg_id){
